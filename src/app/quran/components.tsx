101f5236e5f1a720abe7caaaaf8b59b0e6bf2d7f
al-abd <nexnema@gmail.com>
2025-07-05 16:05:05 +0330

    refactor: update layout and remove unused components; upgrade dependencies in package.json

---
+++ src/app/quran/components.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { forwardRef, useEffect, useState } from "react";
+import { Card, CardProps, Row, RowProps } from "@yakad/ui";
+
+interface FindBarProps extends CardProps {
+    surahnumber: number;
+    ayahnumber: number;
+    pagenumber: number;
+    juz: number;
+    hizb: number;
+}
+export const FindBar = forwardRef<HTMLDivElement, FindBarProps>(
+    ({ surahnumber, ayahnumber, pagenumber, juz, hizb, ...restProps }, ref) => {
+        const [top, setTop] = useState(2);
+        const [lastScrollY, setLastScrollY] = useState(0);
+
+        useEffect(() => {
+            const handleScroll = () => {
+                const currentScrollY = window.scrollY;
+
+                if (currentScrollY > lastScrollY && currentScrollY > 50) {
+                    // Scroll down
+                    setTop(2);
+                } else {
+                    // Scroll up
+                    setTop(8);
+                }
+
+                setLastScrollY(currentScrollY);
+            };
+
+            window.addEventListener("scroll", handleScroll);
+            return () => window.removeEventListener("scroll", handleScroll);
+        }, [lastScrollY]);
+
+        return (
+            <Card
+                ref={ref}
+                {...restProps}
+                blur
+                style={{
+                    position: "sticky",
+                    top: `${top}rem`,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    padding: "1rem 2.5rem",
+                    fontSize: "1.4rem",
+                    boxShadow:
+                        "1.8rem -3.6rem 0 1.8rem rgb(var(--surfaceColor, 254 247 255)), -1.8rem -3.6rem 0 1.8rem rgb(var(--surfaceColor, 254 247 255))",
+                    transition: "top 0.3s ease",
+                    willChange: "top",
+                    zIndex: "1",
+                    cursor: "pointer",
+                }}
+            >
+                <div>{surahnumber + ". Al-Fatihah: " + ayahnumber}</div>
+                <div>{"Page " + pagenumber}</div>
+                <div>{"Juz " + juz + " / " + "Hizb " + hizb}</div>
+            </Card>
+        );
+    }
+);
+FindBar.displayName = "FindBar";
+
+interface PageDividerProps extends RowProps {
+    pagenumber: number;
+}
+export const PageDivider = forwardRef<HTMLDivElement, PageDividerProps>(
+    ({ pagenumber, ...restProps }, ref) => {
+        return (
+            <Row
+                ref={ref}
+                {...restProps}
+                align="center"
+                style={{
+                    padding: "0.5rem",
+                    borderTop: "0.1rem solid #7d7d7d",
+                    fontSize: "1.2rem",
+                    opacity: "0.5",
+                }}
+            >
+                <div style={{ cursor: "pointer" }}>{"Page " + pagenumber}</div>
+            </Row>
+        );
+    }
+);
+PageDivider.displayName = "PageDivider";
--- src/app/quran/page.tsx
+++ src/app/quran/page.tsx
@@ -1,49 +1,185 @@
-import Divider from "@/components/divider/divider";
-import { Container, Stack } from "@yakad/ui";
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Symbol from "@yakad/symbols";
+import {
+    AppBar,
+    Button,
+    Container,
+    Footer,
+    Main,
+    Row,
+    Screen,
+    Stack,
+    Spacer,
+} from "@yakad/ui";
+
+import GoBackButton from "@/components/goBackButton";
+import { FindBar, PageDivider } from "./components";
+import MushafOptionsPopup from "./popupMushafOptions";
+import PlayOptionsPopup from "./popupPlayOptions";
+import FindPopup from "./popupFind";
+import MorePopup from "./popupMore";
 
 const Page: React.FC = () => {
+    const [isFindPopupVisible, setIsFindPopupVisible] =
+        useState<boolean>(false);
+    const [isMorePopupVisible, setIsMorePopupVisible] =
+        useState<boolean>(false);
+    const [isPlayOptionsPopupVisible, setIsPlayOptionsPopupVisible] =
+        useState<boolean>(false);
+    const [isMushafOptionsPopupVisible, setIsMushafOptionsPopupVisible] =
+        useState<boolean>(false);
+
     return (
-        <Container size="md" style={{ padding: "2rem" }}>
-            <Divider />
-            <Stack>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-                <h1>Hello Hello</h1>
-            </Stack>
-            <Divider />
-            <Stack>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-            </Stack>
-            <Stack>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-                <h1>Hello</h1>
-            </Stack>
-        </Container>
+        <Screen>
+            <AppBar style={{ flexWrap: "nowrap" }} sticky autohide>
+                <Container size="md" style={{ padding: 0 }}>
+                    <Row>
+                        <GoBackButton
+                            title="Go back"
+                            icon={<Symbol icon="arrow_back" />}
+                        />
+                        <Spacer />
+                        <h1 style={{ margin: 0 }}>Natiq</h1>
+                        <Spacer />
+                        <Button
+                            title="Mushaf Options"
+                            icon={<Symbol icon="settings" />}
+                            onClick={() => setIsMushafOptionsPopupVisible(true)}
+                        />
+                        <Button
+                            title="More"
+                            icon={<Symbol icon="more_vert" />}
+                            onClick={() => setIsMorePopupVisible(true)}
+                        />
+                    </Row>
+                </Container>
+            </AppBar>
+            <Main>
+                <Container size="md" style={{ padding: "2rem" }}>
+                    <FindBar
+                        surahnumber={1}
+                        ayahnumber={1}
+                        pagenumber={1}
+                        juz={1}
+                        hizb={2}
+                        onClick={() => setIsFindPopupVisible(true)}
+                    />
+                    <Container
+                        size="sm"
+                        align="center"
+                        style={{ padding: "2rem" }}
+                    >
+                        <Row>
+                            <h2 style={{ margin: 0, fontSize: "1.6rem" }}>
+                                1. Al-Fatihah
+                            </h2>
+                            <Spacer />
+                            {/* <SurahPeriodIcon period="makki" /> */}
+                        </Row>
+                        <p>بسم الله الرحن الرحیم</p>
+                    </Container>
+                    <Stack>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                        <h1>Hello Hello</h1>
+                    </Stack>
+                    <PageDivider pagenumber={2} />
+                    <Stack>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                    </Stack>
+                    <PageDivider pagenumber={3} />
+                    <Stack>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                        <h1>Hello</h1>
+                    </Stack>
+                </Container>
+            </Main>
+            <Footer
+                style={{
+                    borderTop:
+                        "0.3rem solid rgb(var(--primaryColor,11 87 208))",
+                    height: "7rem",
+                }}
+                sticky
+                blur
+            >
+                <Container size="md">
+                    <Row style={{ justifyContent: "space-around" }}>
+                        <Button
+                            title="Options"
+                            icon={<Symbol icon="tune" />}
+                            onClick={() => setIsPlayOptionsPopupVisible(true)}
+                        />
+                        <Button
+                            title="Previous Ayah"
+                            icon={<Symbol icon="chevron_left" />}
+                        />
+                        {true ? (
+                            <Button
+                                title="Play"
+                                variant="filled"
+                                icon={<Symbol icon="play_arrow" />}
+                            />
+                        ) : (
+                            <Button
+                                title="Pause"
+                                icon={<Symbol icon="pause" />}
+                            />
+                        )}
+                        <Button
+                            title="Next Ayah"
+                            icon={<Symbol icon="chevron_right" />}
+                        />
+                        <Button
+                            title="Fillscreen"
+                            icon={<Symbol icon="fullscreen" />}
+                        />
+                    </Row>
+                </Container>
+            </Footer>
+            <FindPopup
+                isVisible={isFindPopupVisible}
+                setIsVisible={setIsFindPopupVisible}
+            />
+            <MorePopup
+                isVisible={isMorePopupVisible}
+                setIsVisible={setIsMorePopupVisible}
+            />
+            <MushafOptionsPopup
+                isVisible={isMushafOptionsPopupVisible}
+                setIsVisible={setIsMushafOptionsPopupVisible}
+            />
+            <PlayOptionsPopup
+                isVisible={isPlayOptionsPopupVisible}
+                setIsVisible={setIsPlayOptionsPopupVisible}
+            />
+        </Screen>
     );
 };
 
